b01e67ef866dd24ae075d8bd5134ff2e95137dca
leegiljaelg <todwnlwkdrns@gmail.com>
2024-05-20 15:43:15 +0900

    Add files via upload

---
+++ User.java
@@ -0,0 +1,89 @@
+package login;
+
+public class User {
+	public User() {
+	}
+
+	String id;
+	String pwd;
+	String name;
+	String address;
+	String tel;
+	String gender;
+	String entry_date;
+	String email;
+	String job;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEntry_date() {
+		return entry_date;
+	}
+
+	public void setEntry_date(String entry_date) {
+		this.entry_date = entry_date;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+}
